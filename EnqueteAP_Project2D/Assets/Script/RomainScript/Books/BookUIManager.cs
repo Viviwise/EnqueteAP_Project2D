204e0845da41266fda1f5fad46d4f3541e07cc75
Papi <romain.rousseau21110@gmail.com>
2026-02-26 23:51:57 +0100

    Clicked Elsewhere than Book -> Close it
    Pas actif, juste a accéder au BookUIManager et enlever les /*
    
    Présence à discuter > Possible problème avec Comparaison

--- EnqueteAP_Project2D/Assets/Script/RomainScript/Books/BookUIManager.cs
+++ EnqueteAP_Project2D/Assets/Script/RomainScript/Books/BookUIManager.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using Unity.VisualScripting;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace Script.RomainScript.Books
 {
@@ -9,6 +8,31 @@ namespace Script.RomainScript.Books
         [SerializeField] private BookUI[] booksUI;
 
         private BookUI currentOpenBook;
+        
+        //=============Close Book if clicked elsewhere============//
+
+        /* private void Update()
+        {
+            if (currentOpenBook == null)
+                return;
+            
+            if (Input.GetMouseButtonDown(0))
+            {
+                // Vérifie si on clique sur une UI
+                if (EventSystem.current.IsPointerOverGameObject())
+                {
+                    GameObject clicked = EventSystem.current.currentSelectedGameObject;
+
+                    // Elément du livre → Not Close
+                    if (clicked != null && clicked.transform.IsChildOf(currentOpenBook.transform))
+                        return;
+                }
+                
+                CloseCurrentBook();
+            }
+        }*/
+        
+        //=========================//
 
         public void OpenBook(BookType bookType)
         {
@@ -17,18 +41,24 @@ namespace Script.RomainScript.Books
                 if (ui.BookType == bookType)
                 {
                     if (currentOpenBook == ui)
-                    {
-                        ui.Close();
-                        currentOpenBook = null;
                         return;
-                    }
-                    
+
                     if (currentOpenBook != null)
                         currentOpenBook.Close();
-                    
+
                     ui.Open();
                     currentOpenBook = ui;
+                    return;
                 }
+            }
+        }
+
+        private void CloseCurrentBook()
+        {
+            if (currentOpenBook != null)
+            {
+                currentOpenBook.Close();
+                currentOpenBook = null;
             }
         }
     }
