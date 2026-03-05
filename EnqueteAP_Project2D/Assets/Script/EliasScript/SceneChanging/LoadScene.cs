4871b61205bcd5d98e974b33b0e0fd5153c50c6f
Vivien <viviane-rohaut@outlook.fr>
2026-03-05 14:09:56 +0100

    Fondu au noir + loadScene avec la medium en parametre

--- EnqueteAP_Project2D/Assets/Script/EliasScript/SceneChanging/LoadScene.cs
+++ EnqueteAP_Project2D/Assets/Script/EliasScript/SceneChanging/LoadScene.cs
@@ -1,45 +1,80 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Script.EliasScript
 {
     public class LoadScene : MonoBehaviour
     {
+        public Image fadeImage;
+        private float fadeDuration = 1f;
+
+        private void Start()
+        {
+            StartCoroutine(FadeIn());
+        }
+
+        IEnumerator FadeIn()
+        {
+            float time = 0;
+
+            while (time < fadeDuration)
+            {
+                time += Time.deltaTime;
+                float alpha = 1 - (time / fadeDuration);
+                fadeImage.color = new Color(0, 0, 0, alpha);
+                yield return null;
+            }
+        }
+
+        IEnumerator FadeOutAndLoad(string sceneName)
+        {
+            float time = 0;
+
+            while (time < fadeDuration)
+            {
+                time += Time.deltaTime;
+                float alpha = time / fadeDuration;
+                fadeImage.color = new Color(0, 0, 0, alpha);
+                yield return null;
+            }
+
+            SceneManager.LoadScene(sceneName);
+        }
+
         public void LoadToSceneSalleExam()
         {
-            Debug.Log("button is clicked");
-            SceneManager.LoadScene(2);
+            StartCoroutine(FadeOutAndLoad("SceneSalleExam"));
         }
 
         public void LoadToBrieffingScene()
         {
-            Debug.Log("button is clicked");
-            SceneManager.LoadScene(1);
+            StartCoroutine(FadeOutAndLoad("BriefingScene"));
         }
         
         public void LoadToMenuScene()
         {
-            Debug.Log("button is clicked");
-            SceneManager.LoadScene("MenuScene");
+            StartCoroutine(FadeOutAndLoad("MenuScene"));
         }
 
         public void LoadToGoodEndingScene()
         {
-            Debug.Log("button is clicked");
-            SceneManager.LoadScene("GoodEndScene");
+            StartCoroutine(FadeOutAndLoad("GoodEndScene"));
+        }
+        public void LoadToMediumEndingScene()
+        {
+            StartCoroutine(FadeOutAndLoad("MediumEndScene"));
         }
     
         public void LoadToBadEndingScene()
         {
-            Debug.Log("button is clicked");
-            SceneManager.LoadScene("BadEndScene");
+            StartCoroutine(FadeOutAndLoad("BadEndScene"));
         }
         
         public void QuitScene()
         {
-            Debug.Log("button is clicked");
             Application.Quit();
         }
-
     }
 }
